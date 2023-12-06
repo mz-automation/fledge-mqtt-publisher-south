666f016a9e4c4fed5b4f3ac4bd1e23d781ee42d6
Maurício Fernandes <mauricio.fernandes@gridcorelabs.com>
2023-12-06 15:45:57 +0000

    Finished the implementation of the MQTT Publisher for South plugin
    Signed-off-by: Maurício Fernandes <mauricio.fernandes@gridcorelabs.com>

--- install-plugin.sh
+++ install-plugin.sh
@@ -3,3 +3,5 @@
 pip install paho-mqtt
 sudo cp python/plugins/south/mqtt-publisher/mqtt-publisher.py /usr/local/fledge/python/fledge/plugins/south/mqtt-publisher
 echo Restart fledge to install plugin
+./../../fledge/scripts/fledge stop
+sudo ./../../fledge/scripts/fledge start
--- python/plugins/south/mqtt-publisher/mqtt-publisher.py
+++ python/plugins/south/mqtt-publisher/mqtt-publisher.py
@@ -21,6 +21,7 @@ from fledge.services.core.api import south
 from fledge.services.core.api import north
 import binascii
 import async_ingest
+import json
 
 # MQTT config
 MQTT_BROKER = "localhost"
@@ -33,6 +34,7 @@ _LOGGER = logger.setup(__name__, level=logging.INFO)
 c_callback = None
 c_ingest_ref = None
 loop = None
+
 
 _DEFAULT_CONFIG = {
     'plugin': {
@@ -57,41 +59,27 @@ _DEFAULT_CONFIG = {
         'displayName': 'MQTT Broker Port',
         'mandatory': 'true'
     },
-    'username': {
-        'description': 'Username for broker authentication',
-        'type': 'string',
-        'default': '',
-        'order': '3',
-        'displayName': 'Username'
-    },
-    'password': {
-        'description': 'Password for broker authentication',
-        'type': 'string',
-        'default': '',
-        'order': '4',
-        'displayName': 'Password'
-    },
     'keepAliveInterval': {
         'description': 'Maximum period in seconds allowed between communications with the broker. If no other messages are being exchanged, '
                         'this controls the rate at which the client will send ping messages to the broker.',
         'type': 'integer',
         'default': '60',
-        'order': '5',
+        'order': '3',
         'displayName': 'Keep Alive Interval'
     },
     'topic': {
-        'description': 'The subscription topic to subscribe to receive messages',
+        'description': 'The subscription topic to publish to subscribers',
         'type': 'string',
-        'default': 'Room1/conditions',
-        'order': '6',
-        'displayName': 'Topic To Subscribe',
+        'default': 'PivotCommand',
+        'order': '4',
+        'displayName': 'Topic To Publish',
         'mandatory': 'true'
     },
     'qos': {
         'description': 'The desired quality of service level for the subscription',
         'type': 'integer',
         'default': '0',
-        'order': '7',
+        'order': '5',
         'displayName': 'QoS Level',
         'minimum': '0',
         'maximum': '2'
@@ -100,20 +88,14 @@ _DEFAULT_CONFIG = {
         'description': 'Name of Asset',
         'type': 'string',
         'default': 'mqtt-',
-        'order': '8',
+        'order': '6',
         'displayName': 'Asset Name',
         'mandatory': 'true'
-    },
-    'pollInterval': {
-        'description': 'The interval between poll calls to the device poll routine expressed in milliseconds.',
-        'type': 'integer',
-        'order':'9',
-        'default': '1000'
     },
     'gpiopin': {
         'description': 'The GPIO pin into which the DHT11 data pin is connected',
         'type': 'integer',
-        'order':'10',
+        'order':'7',
         'default': '4'
     }
 }
@@ -126,8 +108,8 @@ def plugin_info():
     return {
         'name':'MQTT Publisher',
         'version':'1.0',
-        'mode':'poll|control',
-        'type': 'south',
+        'mode':'control',
+        'type':'south',
         'sp_control':'',
         'interface': '1.0',
         'config':_DEFAULT_CONFIG
@@ -157,10 +139,8 @@ def plugin_start(handle):
 def plugin_reconfigure(handle, new_config):
     _LOGGER.info('Reconfiguring MQTT south plugin...')
     plugin_shutdown(handle)
-
     new_handle = plugin_init(new_config)
     plugin_start(new_handle)
-
     _LOGGER.info('MQTT south plugin reconfigured.')
     return new_handle
 
@@ -178,96 +158,65 @@ def plugin_shutdown(handle):
     else:
         _LOGGER.info('MQTT south plugin shut down.')
 
-def plugin_operation(handle,operation,count):
-    # Remove non-hexadecimal characters
-    hex_chars = ''.join(char for char in operation if char in '0123456789abcdefABCDEF')
+def plugin_operation(handle, operation, params):
+    x = json.dumps(operation)
+    _LOGGER.debug("plugin_operation(): operation={}, params={}".format(operation, params))
+    params_json = []
 
-    try:
-        # Convert filtered hexadecimal string to bytes
-        operation_bytes = bytes.fromhex(hex_chars)
-        # Decode bytes as UTF-8
-        decoded_operation = operation_bytes.decode('utf-8', errors='replace')
-        _LOGGER.info("OPERATION: %s", decoded_operation)
-        return True
-    except (ValueError, UnicodeDecodeError) as e:
-        _LOGGER.error("Error decoding hexadecimal string: %s", e)
-        return False
+    for param in params:
+        if param[1][0] == '{':
+            param_value = json.loads(param[1])
+        else:
+            param_value = param[1]
 
-def plugin_write(handle, name, value):
-    _LOGGER.info("plugin_write(): name={}, value={}".format(name, value))
+        params_json.append({'name':param[0],'value': param_value})    
+
+    _MQQT_PAYLOAD = {
+        'operation':operation,
+        'parameters':params_json
+    }
+    payload_json = json.dumps(_MQQT_PAYLOAD)
+    handle["_mqtt"].mqtt_client.publish(str(operation),payload_json)
     return True
 
-def plugin_poll(handle):
-    _LOGGER.info("in plugin_poll")
-    return None
+# def plugin_poll(handle):
+#     _LOGGER.info("in plugin_poll")
+#     return None
 
 def plugin_register_ingest(handle, callback, ingest_ref):
     global c_callback, c_ingest_ref
     c_callback = callback
     c_ingest_ref = ingest_ref
 
-def prepare_data():
-    h = float("{0:.2f}".format(random.uniform(50, 100)))
-    t = float("{0:.2f}".format(random.uniform(10, 40)))
-    data = dict()
-    data['humidity'] = h
-    data['temp'] = t
-    jdoc = json.dumps(data)
-    return jdoc
-
-def publish_now(message, client):
-    topic = "Room1/conditions"
-    client.publish(topic, message)
-    _LOGGER.info("Published: " + str(message) + " " + "on MQTT Topic: " + str(topic))
-
-def operation():
-    readings = south.connect.get_readings_async()
-    _LOGGER.info(readings)
-
 class MqttPublisherClient(object):
 
-    __slots__ = ['mqtt_client', 'broker_host', 'broker_port', 'username', 'password', 'topic', 'qos', 'keep_alive_interval', 'asset', 'loop']
+    __slots__ = ['mqtt_client', 'broker_host', 'broker_port', 'topic', 'qos', 'keep_alive_interval', 'asset', 'loop']
 
     def __init__(self, config):
         _LOGGER.info("MQTT Publisher initializing")
         self.mqtt_client = mqtt_client.Client()
         self.broker_host = config['brokerHost']['value']
         self.broker_port = int(config['brokerPort']['value'])
-        self.username = config['username']['value']
-        self.password = config['password']['value']
         self.topic = config['topic']['value']
         self.qos = int(config['qos']['value'])
         self.keep_alive_interval = int(config['keepAliveInterval']['value'])
         self.asset = config['assetName']['value']
+        _LOGGER.info("MQTT Publisher connecting to broker")
+        self.mqtt_client.connect(self.broker_host, self.broker_port, self.keep_alive_interval)
 
     def on_connect(self, client, userdata, flags, rc):
         """ The callback for when the client receives a CONNACK response from the server
         """
-        # operation(userdata,flags,rc)
-        # plugin_list = PluginDiscovery.get_plugins_installed(None,False)
-        # _LOGGER.info(plugin_list)
         client.connected_flag = True
-        message = prepare_data()
-        publish_now(message,client)
 
     def on_disconnect(self, client, userdata, rc):
+        self.mqtt_client.disconnect()
         pass
 
     def on_publish(self, client, data, granted_qos):
         pass
 
-    def on_receive(self):
-        operation()
-
     def start(self):
-        x = north.PluginDiscovery.get_plugins_installed("north",False)
-        _LOGGER.info(x)
-        # z = north.PluginDiscovery.get_plugin_folders("north")
-        # _LOGGER.info(z)
-        y = north.PluginDiscovery.fetch_c_plugins_installed("north",False,"north")
-        _LOGGER.info(y[1])
-        z = north._get_north_schedules(y[1])
-        _LOGGER.info(z)
         _LOGGER.info("MQTT Publisher starting")
         if self.username and len(self.username.strip()) and self.password and len(self.password):
             # no strip on pwd len check, as it can be all spaces?!
@@ -277,8 +226,6 @@ class MqttPublisherClient(object):
 
         self.mqtt_client.on_disconnect = self.on_disconnect
 
-        operation()
-
         self.mqtt_client.connect(self.broker_host, self.broker_port, self.keep_alive_interval)
         _LOGGER.info("MQTT connecting..., Broker Host: %s, Port: %s", self.broker_host, self.broker_port)
 
@@ -287,15 +234,3 @@ class MqttPublisherClient(object):
     def stop(self):
         self.mqtt_client.disconnect()
         self.mqtt_client.loop_stop()
-
-    async def save(self, msg):
-        """Store msg content to Fledge """
-        # TODO: string and other types?
-        payload_json = json.loads(msg.payload.decode('utf-8'))
-        _LOGGER.debug("Ingesting %s on topic %s", payload_json, str(msg.topic)) 
-        data = {
-            'asset': self.asset,
-            'timestamp': utils.local_timestamp(),
-            'readings': payload_json
-        }
-        async_ingest.ingest_callback(c_callback, c_ingest_ref, data)
